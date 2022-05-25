59f41618096191a410722dce3bf35d444f498a34
josep andreu <josepa45@gmail.com>
2022-05-26 01:10:31 +0200

    favicon added

--- src/components/StarShipTotal/StarShipTotal.tsx
+++ src/components/StarShipTotal/StarShipTotal.tsx
@@ -1,11 +1,7 @@
 import { IStarShipTotal } from "../../types/TypeStarShipClass";
 
 const StarShipTotal = ({ number }: IStarShipTotal): JSX.Element => {
-  return (
-    <>
-      <p>{number}</p>
-    </>
-  );
+  return <p>{number}</p>;
 };
 
 export default StarShipTotal;
